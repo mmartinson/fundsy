a8b543c2226a6c98421485f29b52878edb760e7f
Mike Martinson <mike.j.martinson@gmail.com>
2014-11-17 11:12:22 -0800

    Add reject_if to campaign model for nested reward level

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,10 @@ class Campaign < ActiveRecord::Base
   belongs_to :user
   has_many :reward_levels, dependent: :destroy
   accecpts_nested_attributes_for :reward_levels
+    reject_if: proc {|x| x[:price]blank? &&
+                         x[:description]blank? &&
+                         x[:name]blank?
+                    }
 
   validates :title, :description, :deadline, :user_id,  presence: true
   validates :goal, numericality: {greater_than_or_equal_to: 10, allow_nil: true}
